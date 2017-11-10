0c30c492b77dc0348ac8b8d0bfe439e3ca1bb576
Dimitri <xdusster@gmail.com>
2017-11-10 15:52:33 +0100

    interface1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface Result {
+  title : string;
+  author : string;
+  date : number;
+  image : string;
+}
+
+const list: Array<Result> = [{
+  title: "Seigneur", author: "Jackie", date: 2018, image: '<img src="http://lorempixel.com/400/200" />'
+}];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
